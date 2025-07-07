8334dcaa4adc9c753f5c4ccd832d28d12b7fa297
Maxim <119050056+Ambaccador-Gre4ki@users.noreply.github.com>
2025-07-07 16:59:42 +0800

    Update Поиск неактивных учеток.ps1
    Добавил сохранение при выгрузке всех неактивных учёток

--- Поиск неактивных учеток.ps1
+++ Поиск неактивных учеток.ps1
@@ -1,23 +1,30 @@
-﻿function Get-Enabled {
+function Get-Enabled {
 	$user = Read-Host "Введите имя пользователя"
 	Get-ADUser -Identity $user -Properties Enabled, DisplayName | format-table @{Label='Логин'; Expression= {$_.samaccountName}}, @{Label='ФИО'; Expression= {$_.displayName}}, @{Label='Включен'; Expression = {$_.Enabled}}
-	#Get-ADUser -Identity $user -Properties Enabled, DisplayName | Select-Object samaccountName, displayName, Enabled | format-table @{Label='Логин'; Expression= {$_.samaccountName}}, @{Label='ФИО'; Expression= {$_.displayName}}, @{Label='Включен'; Expression = {$_.Enabled}}
+	Write-Host "`n"
 }
 
 function Get-Guide {
 	Write-Host "Всё просто."
 	Write-Host "Для получения статуса учётной записи в домене, нужно в меню нажать 1."
 	Write-Host "После этого ввести логин пользователя, которого нужно проверить."
-	Write-Host "Скрипт покажет статус этой учётки. Если под полем Включен написано True - через эту учётку можно зайти в домен"
+	Write-Host "Для получения списка всех отключенных учеток, нужно в меню нажать 2."
+	Write-Host "`n"		
+	Write-Host "Скрипт покажет статус учётки. Если под полем Включен написано True - через эту учётку можно зайти в домен"
 	Write-Host "Если же написано False - то она отключена и зайти с её помощью не получится."
+	Write-Host "`n"	
 }
 
 function Get-Enabled-All {
-	Get-ADUser -Filter {Enabled -eq "False"} | format-table @{Label='Логин'; Expression= {$_.samaccountName}}, @{Label='ФИО'; Expression= {$_.Name}}, @{Label='Включен'; Expression = {$_.Enabled}}
+	$result = Get-ADUser -Filter {Enabled -eq "False"} | format-table @{Label='Логин'; Expression= {$_.samaccountName}}, @{Label='ФИО'; Expression= {$_.Name}}, @{Label='Включен'; Expression = {$_.Enabled}}
+	Write-Output $result
+	Out-File -FilePath c:\users\bogdashov\desktop\results.txt -InputObject $result
+	Write-Host "Выгрузка была сохранена на рабочем столе с именем results.txt" -foregroundcolor yellow
+	Write-Host "`n"
 }
 
 :menuLoop while ($true) {
-    Write-Host "=====" -noNewline
+    	Write-Host "=====" -noNewline
 	Write-Host "Меню" -foregroundcolor red -backgroundcolor yellow -noNewline
 	Write-Host "====="
 	Write-Host "1) " -noNewline
@@ -32,7 +39,7 @@ function Get-Enabled-All {
     switch (Read-Host "`nВыберите пункт меню") {
         '1' {Get-Enabled}
         '2' {Get-Enabled-All}
-		'3' {Get-Guide}
+	'3' {Get-Guide}
         '8' {break menuLoop}
     }
 }
